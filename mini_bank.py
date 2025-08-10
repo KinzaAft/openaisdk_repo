1cd49ceb6175c8775156789bb52db2b77bf59693
Kinza Aftab <160029911+KinzaAft@users.noreply.github.com>
2025-08-11 00:15:38 +0500

    Update mini_bank.py

--- mini_bank.py
+++ mini_bank.py
@@ -1,79 +1,3 @@
-# from agents import Agent, Runner, OpenAIChatCompletionsModel, input_guardrail,set_tracing_disabled
-# from openai import AsyncOpenAI
-# import os
-# from dotenv import load_dotenv
-# from pydantic import BaseModel
-# from agents import (InputGuardrail,
-#                     RunContextWrapper,
-#                     TResponseInputItem,
-#                     GuardrailFunctionOutput,
-#                     InputGuardrailTripwireTriggered
-#                     )
-# import chainlit as cl 
-
-# load_dotenv()
-# set_tracing_disabled(disabled=True)
-
-# gemini_api_key=os.getenv("GEMINI_API_KEY")
-
-# client= AsyncOpenAI(
-#     api_key=gemini_api_key,
-#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-# )
-
-# model= OpenAIChatCompletionsModel(
-#     model="gemini-2.0-flash",
-#     openai_client=client,
-# )
-# class Outputbank(BaseModel):
-#     is_banking_related: bool
-#     reasoning: str
-
-# input_guardrials_agents= Agent(
-#     name="Input Guardrails Checker",
-#     instructions="Check if the user question is banking related,retrun true,if the quetion is not related to banking, return flase",
-#     model=model,
-#     output_type=Outputbank
-# )
-
-# @input_guardrail
-# async def input_guardrail_functions(
-#     ctx: RunContextWrapper,
-#     agent: Agent,
-#     input: str | list[TResponseInputItem]
-# )->GuardrailFunctionOutput:
-#     result= await Runner.run(
-#        input_guardrials_agents,
-#        input
-#     )
-#     return GuardrailFunctionOutput(
-#         output_info=result.final_output,
-#         tripwire_triggered=not result.final_output.is_banking_related,
-#     )
-
-# trige_agent= Agent(
-#     name="Banking Agent",
-#     instructions="you are a banking agent you only respond about banking related question",
-#     model=model,
-#     input_guardrails=[input_guardrail_functions]
-# )
-
-# @cl.on_chat_start
-# async def on_chat_start():
-#     await cl.Message(content="I'm ready to assist you").send()
-
-# @cl.on_message
-# async def on_message(message:cl.Message):
-#     try:
-#         result= await Runner.run(
-#             trige_agent,
-#             input=message.content
-#         )
-#         await cl.Message(content=result.final_output).send()
-#     except InputGuardrailTripwireTriggered:
-#         await cl.Message(content="Please Ask Banking Questions").send()
-# bank_agent_project.py
-
 from agents import Agent, Runner, OpenAIChatCompletionsModel, input_guardrail, output_guardrail, set_tracing_disabled
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
@@ -172,3 +96,4 @@ async def on_message(message: cl.Message):
         await cl.Message(content=str(result.final_output)).send()
     except InputGuardrailTripwireTriggered:
         await cl.Message(await cl.Message(content="Please Ask Banking Questions").send())
+
